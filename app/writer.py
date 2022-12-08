eb96e9931fb69e6421b64a74370bd12bfa86eb87
Samuel Braga <samuel.fantini.braga@hotmail.com>
2022-12-08 07:22:02 -0300

    Adding features to writer module

--- app/writer.py
+++ app/writer.py
@@ -6,6 +6,12 @@ class IWriter():
     @abstractmethod
     def write_file(self, table_name: str, content: str) -> None:
         """Method responsible to write on file"""
+    @abstractmethod
+    def remove_file(self, table_name: str) -> None:
+        """Method responsible to delete file"""
+    @abstractmethod
+    def remove_last_character(self, table_name: str) -> None:
+        """Method responsible to remove last character from text file"""
 
 
 class Writer(IWriter):
@@ -23,3 +29,10 @@ class Writer(IWriter):
         file_path = self.file_path_template.format(table_name=table_name)
         if os.path.exists(file_path):
             os.remove(file_path)
+
+    def remove_last_character(self, table_name: str) -> None:
+        file_path = self.file_path_template.format(table_name=table_name)
+        file = open(file_path, "rb+")
+        file.seek(-2, os.SEEK_END)
+        file.truncate()
+        file.close()
